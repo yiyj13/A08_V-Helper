380c46e618b3fab4e75b72a0780e81aebeb56693
杨朗 <yanglang21@mails.tsinghua.edu.cn>
2023-11-23 01:01:54 +0800

    add mockjs

--- config/dev.ts
+++ config/dev.ts
@@ -1,9 +1,20 @@
 module.exports = {
+  plugins: ['@tarojs/plugin-mock'],
   env: {
     NODE_ENV: '"development"'
   },
   defineConstants: {
   },
   mini: {},
-  h5: {}
+  h5: {
+    devServer: {
+      proxy: {
+        '/api': {
+          target: process.env.TARO_APP_API,
+          changeOrigin: true,
+          pathRewrite: { '^/api': '' },
+        },
+      },
+    },
+  }
 }
