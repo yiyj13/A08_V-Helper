b1de0cf4c983dbfa775909b9adaab3fa61b2b477
杨朗 <yanglang21@mails.tsinghua.edu.cn>
2023-11-26 22:52:37 +0800

    add brand color to the tailwind palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,4 +9,12 @@ module.exports = {
     // 小程序不需要 preflight，因为这主要是给 h5 的，如果你要同时开发多端，你应该使用 process.env.TARO_ENV 环境变量来控制它
     preflight: false,
   },
+  theme: {
+    extend: {
+      colors: {
+        // add our brand color to the palette
+        brand: '#176b87',
+      },
+    },
+  },
 }
