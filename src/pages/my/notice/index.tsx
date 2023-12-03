e8b27b8d437dc865a71235093066cf1a5d87f004
tzh <3136374549@qq.com>
2023-12-03 18:10:07 +0800

    Enhance: notice page

--- src/pages/my/notice/index.tsx
+++ src/pages/my/notice/index.tsx
@@ -1,6 +1,16 @@
 import { Checkbox } from '@nutui/nutui-react-taro';
+import { useEffect, useState } from 'react'
+import { Loading } from '@nutui/nutui-react-taro'
+
+import api from '../../../api'
 
 export default function Index () {
+  const [showNotice, setShowNotice] = useState<boolean>(true)
+  
+  const handleShowNotice = (state) => {
+    setShowNotice(state)
+  }
+
   return (
     <div className='flex flex-col items-center h-screen'>
       <div className='flex flex-col h-11/12 w-11/12 m-4 rounded-2xl std-box-shadow'>
@@ -8,97 +18,30 @@ export default function Index () {
         <div className="flex flex-row-reverse m-4">
           <div className='p-2'>
             <Checkbox
-              labelPosition="left" label="开启消息通知"
+              labelPosition="left"
+              label="开启消息通知"
               defaultChecked={true}
+              onChange={handleShowNotice}
             />
           </div>
         </div>
-        
-        <div className='h-[75vh] overflow-auto m-4'>
-          <ListItem
-            profile='张三'
-            vaccine='新冠疫苗'
-            type='第一针'
-            time='2021-09-01' />
-          <ListItem
-            profile='张三'
-            vaccine='新冠疫苗'
-            type='第一针'
-            time='2021-09-01' />
-          <ListItem
-            profile='张三'
-            vaccine='新冠疫苗'
-            type='第一针'
-            time='2021-09-01' />
-          <ListItem
-            profile='张三'
-            vaccine='新冠疫苗'
-            type='第一针'
-            time='2021-09-01' />
-          <ListItem
-            profile='张三'
-            vaccine='新冠疫苗'
-            type='第一针'
-            time='2021-09-01' />
-          <ListItem
-            profile='张三'
-            vaccine='新冠疫苗'
-            type='第一针'
-            time='2021-09-01' />
-          <ListItem
-            profile='张三'
-            vaccine='新冠疫苗'
-            type='第一针'
-            time='2021-09-01' />
-          <ListItem
-            profile='张三'
-            vaccine='新冠疫苗'
-            type='第一针'
-            time='2021-09-01' />
-          <ListItem
-            profile='张三'
-            vaccine='新冠疫苗'
-            type='第一针'
-            time='2021-09-01' />
-          <ListItem
-            profile='张三'
-            vaccine='新冠疫苗'
-            type='第一针'
-            time='2021-09-01' />
-          <ListItem
-            profile='张三'
-            vaccine='新冠疫苗'
-            type='第一针'
-            time='2021-09-01' />
-          <ListItem
-            profile='张三'
-            vaccine='新冠疫苗'
-            type='第一针'
-            time='2021-09-01' />
-          <ListItem
-            profile='张三'
-            vaccine='新冠疫苗'
-            type='第一针'
-            time='2021-09-01' />
-          <ListItem
-            profile='张三'
-            vaccine='新冠疫苗'
-            type='第一针'
-            time='2021-09-01' />
-        </div>
+
+        {showNotice && <NoticeList />}
+        {/* <NoticeList /> */}
+
       </div>
     </div>
   )
 }
 
-interface IListItem {
+type TNoticeItem = {
   profile: string;
   vaccine: string;
   type: string;
-  time: string;
+  date: string;
 }
 
-function ListItem(props: IListItem) {
+function ListItem(props: TNoticeItem) {
   return (
     <div className='flex flex-row justify-evenly items-center m-2 p-2 rounded-2xl bg-gradient-to-r from-teal-600 to-teal-400 text-white'>
 
@@ -109,8 +52,36 @@ function ListItem(props: IListItem) {
           <div className='text-sm'>{props.type}</div>
         </div>
         
-        <div className='text-sm'>{props.time}</div>
+        <div className='text-sm'>{props.date}</div>
 
+    </div>
+  )
+}
+
+function NoticeList() {
+  const [noticeList, setNoticeList] = useState<TNoticeItem[] | null>()
+  useEffect(() => {
+    api.request({ url: '/api/notices' }).then((res) => {
+      const result = res.data as TNoticeItem[]
+      setNoticeList(result)
+    })
+  }, [])
+
+  if (!noticeList) {
+    return <Loading className='h-screen w-screen' />
+  }
+
+  return (
+    <div className='flex flex-col h-[75vh] overflow-auto m-4'>
+      {noticeList.map((item, index) => (
+        <ListItem
+          key={index}
+          profile={item.profile}
+          vaccine={item.vaccine}
+          type={item.type}
+          date={item.date}
+        />
+      ))}
     </div>
   )
 }
