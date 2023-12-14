284d185a44684fead822692210800de72e8e6df3
杨朗 <yanglang21@mails.tsinghua.edu.cn>
2023-12-14 10:08:39 +0800

    update(send post): automatically select 'vaccine' tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
+import { useState, PropsWithChildren } from 'react'
 import { useTabStore, useUserStore } from '../models'
 
 import HomePage from '../pages/home'
 import EnquiryPage from '../pages/enquiry'
-import DatePage from '../pages/date'
+import CommunityPage from '../pages/community'
 import ProfilePage from '../pages/my'
 import CustomTabBar from '../tabbar'
 
@@ -10,22 +11,30 @@ import LoginPage from '../pages/login'
 
 export default function Index() {
   const isLogged = useUserStore.use.isLogged()
-  const tabIndex = useTabStore((state) => state.tabIndex)
-  const setTabIndex = useTabStore((state) => state.setTabIndex)
 
-  if (!isLogged) {
-    setTabIndex(0)
-    return <LoginPage />
-  }
+  if (!isLogged) return <LoginPage />
 
   return (
     <div>
-      {[HomePage, EnquiryPage, DatePage, ProfilePage].map((Content, index) =>
-        (tabIndex <= 2 && tabIndex === index) || (tabIndex >= 3 && tabIndex === index + 1) ? (
-          <Content key={index} />
-        ) : null
-      )}
+      {[HomePage, EnquiryPage, CommunityPage, ProfilePage].map((Content, index) => (
+        <LazyLoadTab tabIndex={index > 1 ? index + 1 : index} key={index}>
+          <Content />
+        </LazyLoadTab>
+      ))}
       <CustomTabBar />
     </div>
   )
+}
+
+function LazyLoadTab(props: PropsWithChildren<{ tabIndex: number }>) {
+  const tabIndex = useTabStore((state) => state.tabIndex)
+  const [loaded, setLoaded] = useState(false)
+
+  if (props.tabIndex != tabIndex && !loaded) {
+    return null
+  } else if (!loaded) {
+    setLoaded(true)
+  }
+
+  return <div style={{ display: tabIndex === props.tabIndex ? 'block' : 'none' }}>{props.children}</div>
 }
