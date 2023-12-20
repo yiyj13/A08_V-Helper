9ccc1cc75a44fc8c5a88dd1b9b80411b883295d2
YujieYi <YujieYi13@outlook.com>
2023-12-20 09:24:12 +0800

    fix: form logic refinement

--- src/api/methods.tsx
+++ src/api/methods.tsx
@@ -41,13 +41,15 @@ export type ArticleFull = Article & {
 export type VaccinationRecord = GinBase & {
   profileId: number
   vaccineId: number
-  vaccineType: string
+  vaccinationType: string
   vaccine: Vaccine
   vaccinationDate: string
   voucher: string
   vaccinationLocation: string
   reminder: boolean
   remindTime: string
+  valid: string // 有效期 6月 1年
+  remindBefore: string // 提前多久提醒 2天 1周 
   nextVaccinationDate: string
   note: string
 }
