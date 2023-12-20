c076fc75d229a6fc6652742ab7b1ae3ef245da2d
杨朗 <yanglang21@mails.tsinghua.edu.cn>
2023-12-20 11:58:06 +0800

    update: post filter in effect

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { memo, useState } from 'react'
 import { Skeleton } from '@nutui/nutui-react-taro'
 import { ArrowDown } from '@nutui/icons-react-taro'
 import useSWRInfinite from 'swr/infinite'
@@ -9,14 +9,30 @@ import { ArticlePreview } from './ArticlePreview'
 import api, { Article, useVaccines } from '../../api/'
 import { PullDownRefresh } from '../../components/pulldownrefresh'
 import { NetworkError } from '../../components/errors'
+import { useCommunityStore } from '../../models'
 
 const PAGESIZE = 5
 
+export default function Index() {
+  return (
+    <>
+      <Header />
+      <Community />
+    </>
+  )
+}
+
 // TODO: virtualList optimization
-export default function Community() {
+export function Community(props: { filter?: number }) {
+  const filter = useCommunityStore.use.vaccineId()
+
   const { data, isLoading, size, setSize, error } = useSWRInfinite(
-    (index) => `/api/articles?page=${index + 1}&size=${PAGESIZE}`,
-    (url) => api.get(url).then((res) => res.data)
+    (index) =>
+      filter
+        ? `/api/articles?page=${index + 1}&size=${PAGESIZE}&isBind=true&vaccineID=${filter}`
+        : `/api/articles?page=${index + 1}&size=${PAGESIZE}`,
+    (url) => api.get(url).then((res) => res.data),
+    { revalidateIfStale: false }
   )
 
   const articles: Article[] = data ? [].concat(...data) : []
@@ -31,20 +47,17 @@ export default function Community() {
   useReachBottom(() => shouldLoadMore && loadMore())
 
   return (
-    <div>
-      <Header />
-      <PullDownRefresh onRefresh={() => setSize(1)}>
-        <main className='px-8 pb-32 mt-4'>
-          <div className='flex flex-col gap-6'>
-            {error && <NetworkError></NetworkError>}
-            {articles?.map((article, index) => (
-              <ArticlePreview key={index} {...article} />
-            ))}
-            {isLoadingMore && !error && <Skeletons />}
-          </div>
-        </main>
-      </PullDownRefresh>
-    </div>
+    <PullDownRefresh onRefresh={() => setSize(1)}>
+      <main className='px-8 pb-32 mt-4'>
+        <div className='flex flex-col gap-6'>
+          {error && <NetworkError></NetworkError>}
+          {articles?.map((article, index) => (
+            <ArticlePreview key={index} {...article} />
+          ))}
+          {isLoadingMore && !error && <Skeletons />}
+        </div>
+      </main>
+    </PullDownRefresh>
   )
 }
 
@@ -60,7 +73,8 @@ const Skeletons = () => (
 const Header = () => {
   const { data } = useVaccines()
   const [showFilter, setShowFilter] = useState<boolean>(false)
-  const [filter, setFilter] = useState<number | undefined>(undefined)
+  const filter = useCommunityStore.use.vaccineId()
+  const toggleFilter = useCommunityStore.use.toggleFilter()
 
   return (
     <header
@@ -71,7 +85,7 @@ const Header = () => {
     >
       <nav
         id='foo'
-        className={clsx('fixed z-20 w-full bg-white shadow-md shadow-gray-600/5 md:relative md:bg-transparent', {})}
+        className={clsx('fixed z-20 w-full bg-white md:relative md:bg-transparent', {})}
       >
         <div className='m-auto px-6 md:px-12'>
           <div className='flex flex-wrap items-center justify-between gap-6 py-3'>
@@ -100,10 +114,14 @@ const Header = () => {
           {data?.map((vaccine, index) => (
             <li
               key={index}
-              className={clsx('flex items-center justify-center px-2 py-2 text-sm font-medium rounded-full', {
-                'bg-slate-100 text-brand': filter === vaccine.ID,
-              })}
-              onClick={() => setFilter((f) => (f === vaccine.ID ? undefined : vaccine.ID))}
+              className={clsx(
+                'flex items-center justify-center px-2 py-2 text-sm font-medium rounded-full',
+                'transition-colors',
+                {
+                  'bg-slate-100 text-brand': filter === vaccine.ID,
+                }
+              )}
+              onClick={() => toggleFilter(vaccine.ID)}
             >
               {vaccine.name}
             </li>
