8b2faaad843bf5deb464736182205979cebda04f
杨朗 <yanglang21@mails.tsinghua.edu.cn>
2023-12-26 23:24:40 +0800

    feat: Integrate posts into vaccine details page & UI imporvement

--- src/pages/vacDetails/index.tsx
+++ src/pages/vacDetails/index.tsx
@@ -1,15 +1,12 @@
 import { useState } from 'react'
 import { useRouter } from 'taro-hooks'
 import { ScrollView, Text } from '@tarojs/components'
-import { Follow, HeartFill1, Uploader } from '@nutui/icons-react-taro'
 import { Loading } from '@nutui/nutui-react-taro'
-import Taro from '@tarojs/taro'
 import clsx from 'clsx'
-import useSWR from 'swr'
 
-import Tab from './tab'
-import { followVaccine, getVaccineByID, unfollowVaccine } from '../../api/methods'
-import { useUserFollowing } from '../../api/hooks'
+import Header from './header'
+import DiscussionBlock from './discussion'
+import { useVaccines } from '../../api'
 
 export default function Index() {
   const [route] = useRouter()
@@ -17,62 +14,43 @@ export default function Index() {
 
   const [tabIdx, setTabIdx] = useState(0)
 
-  const { data: vaccine } = useSWR('/api/vaccines/' + id, () => getVaccineByID(id))
+  const { data } = useVaccines()
+  const vaccine = data?.find((v) => v.ID === parseInt(id))
+
+  const activeId = 'detailTab' + tabIdx
 
   if (!vaccine) return <Loading className='w-screen h-screen'></Loading>
 
-  const tabs = ['疫苗介绍', '注意事项', '目标病症', '大家讨论']
-  const activeId = 'detailTab' + tabIdx
-
   return (
     <div className='flex h-screen w-screen flex-col'>
-      <div className='flex py-8 w-full flex-row items-center justify-between'>
-        <label className='ml-4 text-2xl font-bold'>{vaccine.name}</label>
-        <div className='mr-4 flex flex-row'>
-          <FollowButton vaccine_id={vaccine.ID} />
-        </div>
-      </div>
-      <div className='flex h-full w-full flex-col rounded-3xl bg-slate-100'>
-        <Tab titles={tabs} value={tabIdx} setValue={setTabIdx} />
-        <ScrollView className='self-center h-[72vh]' scrollY scrollWithAnimation scrollIntoView={activeId}>
-          <DetailBlock
-            id='detailTab0'
-            title='疫苗介绍'
-            paragraph={vaccine.description}
-            activeId={activeId}
-          ></DetailBlock>
-          <DetailBlock
-            id='detailTab1'
-            title='注意事项'
-            paragraph={vaccine.precautions + '\n' + vaccine.sideEffects}
-            activeId={activeId}
-          ></DetailBlock>
-          <DetailBlock
-            id='detailTab2'
-            title='目标病症'
-            paragraph={vaccine.targetDisease}
-            activeId={activeId}
-          ></DetailBlock>
-          <DetailBlock id='detailTab3' title='大家讨论' paragraph='暂无' activeId={activeId}></DetailBlock>
-          <div
-            className={clsx('flex justify-center items-center rounded-2xl mx-4 py-2 bg-slate-200', 'active:scale-105')}
-            onClick={() => Taro.navigateTo({ url: '/pages/sendpost/index?vacName=' + vaccine.name })}
-          >
-            <Uploader className='text-brand'></Uploader>
-          </div>
-        </ScrollView>
-      </div>
+      <Header activeId={tabIdx} vaccine_id={vaccine.ID} setActiveId={setTabIdx} />
+      <ScrollView style={{ height: 'calc(100vh - 7rem)' }} scrollY scrollWithAnimation scrollIntoView={activeId}>
+        <DetailBlock
+          id='detailTab0'
+          title='疫苗介绍'
+          paragraph={vaccine?.description}
+          activeId={activeId}
+        ></DetailBlock>
+        <DetailBlock
+          id='detailTab1'
+          title='注意事项'
+          paragraph={vaccine.precautions + '\n' + vaccine.sideEffects}
+          activeId={activeId}
+        ></DetailBlock>
+        <DetailBlock
+          id='detailTab2'
+          title='目标病症'
+          paragraph={vaccine.targetDisease}
+          activeId={activeId}
+        ></DetailBlock>
+        <DetailBlock id='detailTab3' title='大家讨论' activeId={activeId}></DetailBlock>
+        <DiscussionBlock vaccine_id={vaccine.ID} />
+      </ScrollView>
     </div>
   )
 }
 
-type BlockProps = {
-  id: string
-  title: string
-  paragraph: string
-  activeId: string
-}
-function DetailBlock(props: BlockProps) {
+function DetailBlock(props: { id: string; title: string; paragraph?: string; activeId: string }) {
   return (
     <div id={props.id} className='p-4'>
       <header
@@ -82,25 +60,13 @@ function DetailBlock(props: BlockProps) {
       >
         {props.title}
       </header>
-      <Text className='text-base'>{props.paragraph}</Text>
+      {props.paragraph ? (
+        <div className='px-2'>
+          <Text userSelect className='text-base'>
+            {props.paragraph}
+          </Text>
+        </div>
+      ) : null}
     </div>
-  )
-}
-
-const FollowButton = (props: { vaccine_id: number }) => {
-  const { data: user, mutate } = useUserFollowing()
-  const handleFollowClick = async () => {
-    await followVaccine(props.vaccine_id)
-    mutate()
-  }
-  const handleUnfollowClick = async () => {
-    await unfollowVaccine(props.vaccine_id)
-    mutate()
-  }
-  const following = user?.followingVaccines?.find((v) => v.ID === props.vaccine_id) !== undefined
-  return following ? (
-    <HeartFill1 className='text-brand' size={16} onClick={handleUnfollowClick}></HeartFill1>
-  ) : (
-    <Follow className='text-brand' size={16} onClick={handleFollowClick}></Follow>
   )
 }
